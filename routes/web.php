b84aef5471401237a284bd5673a5d1402449f5b5
rhelzz <muhammadrasyad1004@gmail.com>
2026-06-28 00:54:50 +0700

    feat(routes): add dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/dashboard', DashboardController::class)->name('dashboard');
